076e14f6a0396fb1e8a1b4b57e68dfbacd4b7531
Hugo Pereira <hugopereira.mg@gmail.com>
2022-09-07 19:36:12 -0300

    V2.1

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Hello, World!");
 //V1.0
 //V2.0 
 Console.WriteLine("Lucas");
+
+//V2.1
+WriteCubes();
+
+void WriteCubes()
+{
+  Console.WriteLine(Cube(3));    // 27
+  Console.WriteLine(Cube(4));    // 64
+  Console.WriteLine(Cube(5));    // 125
+
+  int Cube(int value) => value * value * value;   // Método local
+}
