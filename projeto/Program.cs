fbbbd5ffb03a2a5f6b166aee895d2e98e6400d1f
Hugo Pereira <hugopereira.mg@gmail.com>
2022-09-07 19:36:12 -0300

    V2.1

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -11,3 +11,15 @@ Console.WriteLine("Lucas - V4.0");
 Console.WriteLine("Lucas - V3.1");
 //3.2
 Console.WriteLine("Lucas - V3.2");
+
+//V2.1
+WriteCubes();
+
+void WriteCubes()
+{
+  Console.WriteLine(Cube(3));    // 27
+  Console.WriteLine(Cube(4));    // 64
+  Console.WriteLine(Cube(5));    // 125
+
+  int Cube(int value) => value * value * value;   // Método local
+}
